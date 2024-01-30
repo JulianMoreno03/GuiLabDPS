f14e4dad7783111392cf3990e497304649f8697f
Julian Moreno <juliancastillo204@gmail.com>
2024-01-30 14:52:58 -0600

    Guia Terminada

--- Guia2/guia2/src/app/page.js
+++ Guia2/guia2/src/app/page.js
@@ -1,11 +1,17 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
+
+const msj = "hola mundo";
 
 export default function Home() {
+const [numero1,setNumero] = useState();
+
   return (
     <main className={styles.main}>
 
-    <h1 className="">hola mudno</h1>
+    <h1>{msj}</h1>
          
     </main>
   );
